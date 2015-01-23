bd4f48650d94eba6ada5d074d847bb356c468196
Rafael Chargel <rchargel@chariotsolutions.com>
2015-01-23 08:49:06 -0500

    First commit

---
+++ localiday.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"strconv"
+	"time"
+
+	"github.com/rchargel/localiday/db"
+	"github.com/rchargel/localiday/domain"
+	"github.com/rchargel/localiday/server"
+)
+
+func main() {
+	start := time.Now()
+	config := server.LoadConfiguration()
+	fmt.Println(config.ToString())
+
+	cores := runtime.NumCPU()
+	runtime.GOMAXPROCS(cores)
+	log.Printf("Running on %v cores.\n", cores)
+	sport := os.Getenv("PORT")
+
+	port, err := strconv.ParseUint(sport, 10, 16)
+	if err != nil {
+		log.Panic(err)
+	}
+	err = db.NewDatabase("postgres", "postgres", "localhost", "localiday")
+	if err != nil {
+		log.Fatal("Could not connect to database.", err)
+	}
+	err = domain.BootStrap()
+	if err != nil {
+		log.Panic("Could not bootstrap database.", err)
+	}
+	appServer := server.AppServer{Port: uint16(port)}
+	appServer.Start()
+
+	log.Printf("Application started in %v.", time.Since(start))
+}
